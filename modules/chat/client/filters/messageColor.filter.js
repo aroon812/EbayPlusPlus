d9536f631b4742d6d5f49ffcb72170c1876e9256
Aaron Thompson <athompson812@hotmail.com>
2018-04-27 11:21:32 -0700

    small changes in the chat filter

--- modules/chat/client/filters/messageColor.filter.js
+++ modules/chat/client/filters/messageColor.filter.js
@@ -7,18 +7,18 @@
    messageColor.$inject = ['Authentication'];
 
     function messageColor (message) {
-        
-        console.log(message.user.username);
-        return function (doc) {
-            $( "li#element" ).each(function() {
-                if (message.user.username == 'temp'){
-                    this.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
-                }
-                else{
-                    this.style.backgroundColor = 'rgba(0, 0, 255, 0.2)';
-                }
-                
-            })
+      console.log(user.username);
+      console.log(message.user);
+        return function () {
+          $( "li#element" ).each(function() {
+            
+            if (message.user.username === user.username){
+              this.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
+            }
+            else{
+              this.style.backgroundColor = 'rgba(0, 0, 255, 0.2)';
+            }
+          });
         };
       }
 
